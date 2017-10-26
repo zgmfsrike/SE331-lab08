e48c9d71c153ff9c7fb047c71a8f1722e4bb1265
nuBoreZ <kritsanah_supphasri@cmu.ac.th>
2017-10-26 22:12:02 +0700

    2.5 finish

--- src/main/java/camt/cbsd/lab05/entity/Student.java
+++ src/main/java/camt/cbsd/lab05/entity/Student.java
@@ -1,7 +1,13 @@
 package camt.cbsd.lab05.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = false)
 public class Student {
     long id;
@@ -14,59 +20,24 @@ public class Student {
     int penAmount;
     String description;
 
-    public String getName() {
-        return name;
-    }
 
-    public String getSurname() {
-        return surname;
-    }
 
     public long getId() {
         return id;
     }
 
-    public String getStudentId() {
-        return studentId;
-    }
 
-    public double getGpa() {
-        return gpa;
-    }
 
     public String getImage() {
         return image;
     }
 
-    public boolean isFeature() {
-        return feature;
-    }
 
-    public int getPenAmount() {
-        return penAmount;
-    }
-
-    public String getDescription() {
-        return description;
-    }
 
     public void setImage(String image) {
         this.image = image;
     }
 
-    public Student(){
 
-    }
-    public Student(long id, String studentId, String name, String surname, double gpa, String image, boolean feature, int penAmount, String description) {
-        this.id = id;
-        this.studentId = studentId;
-        this.name = name;
-        this.surname = surname;
 
-        this.gpa = gpa;
-        this.image = image;
-        this.feature = feature;
-        this.penAmount = penAmount;
-        this.description = description;
-    }
 }
